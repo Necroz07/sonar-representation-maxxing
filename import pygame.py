d51a6b7a5385f70439231043842f1356cf016437
Shrey Shah <rizzcularsuccess@gmail.com>
2026-02-07 15:50:18 +0530

    Update import pygame.py

--- import pygame.py
+++ import pygame.py
@@ -1,1 +1,33 @@
 import pygame
+import math
+import time
+
+pygame.init()
+
+screen=pygame.display.set_mode((800,800))
+
+clock=pygame.time.Clock()
+
+running=True
+
+counter = 0
+
+while running:
+
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
+        
+
+
+
+        
+    
+    
