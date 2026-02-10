3b8a258bbb5cb3ef30c0747f9219997441f008f3
Shrey Shah <rizzcularsuccess@gmail.com>
2026-02-10 14:13:38 +0530

    added in assets and sprites

--- import pygame.py
+++ import pygame.py
@@ -8,8 +8,18 @@ pygame.init()
 
 font = pygame.font.SysFont(None, 169)
 
-screen=pygame.display.set_mode((800,800))
-pygame.display.set_caption("Timer Test!!!! goooooon")
+screen=pygame.display.set_mode((600,600))
+pygame.display.set_caption("goooooon")
+
+bg = pygame.image.load("background.png").convert()
+bg = pygame.transform.scale(bg, (600,600))
+
+obj = pygame.image.load("object.png").convert_alpha()
+obj = pygame.transform.scale(obj, (50, 50))
+
+
+wave_surf = pygame.Surface((600, 600), pygame.SRCALPHA)
+
 
 clock=pygame.time.Clock()
 
@@ -17,7 +27,16 @@ running=True
 
 counter = 0
 
+wave_radius = 0
+
+wave_speed = 4
+
+
 last_time = pygame.time.get_ticks()
+
+
+
+
 
 
 while running:
@@ -27,23 +46,40 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+            
+
     current_time = pygame.time.get_ticks()
 
+    
+
     if current_time - last_time >=1000:
-
-
-        print(counter)
+        
         counter+=1
         last_time = current_time
 
         if counter > 10:
             running = False
 
-    screen.fill("purple")
+
+    wave_surf = pygame.Surface((600,600), pygame.SRCALPHA)
+
+    pygame.draw.circle(wave_surf, (0, 255, 255, 120), (275, 275), wave_radius, 3)
+
+            
+
+    screen.blit(bg, (0,0))
+    screen.blit(obj, (275,275))
+
+    wave_radius += wave_speed
+
+
+    if wave_radius > 900:
+        wave_radius = 0
+    
 
     text = font.render(str(counter), True, "black")
 
-    text_rect = text.get_rect(center=(400,400))
+    text_rect = text.get_rect(center=(300,300))
 
     
         
@@ -56,7 +92,6 @@ while running:
     clock.tick(60)  # limits FPS to 60
 
 pygame.quit()
-        
 
 
 
