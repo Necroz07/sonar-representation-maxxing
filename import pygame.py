a7042435ba0c42b214472f4f680cfc0240b09a09
Shrey Shah <rizzcularsuccess@gmail.com>
2026-02-10 15:53:15 +0530

    wave animation

--- import pygame.py
+++ import pygame.py
@@ -11,6 +11,8 @@ font = pygame.font.SysFont(None, 169)
 screen=pygame.display.set_mode((600,600))
 pygame.display.set_caption("goooooon")
 
+wave_surf = pygame.Surface((600, 600), pygame.SRCALPHA)
+
 bg = pygame.image.load("background.png").convert()
 bg = pygame.transform.scale(bg, (600,600))
 
@@ -18,18 +20,15 @@ obj = pygame.image.load("object.png").convert_alpha()
 obj = pygame.transform.scale(obj, (50, 50))
 
 
-wave_surf = pygame.Surface((600, 600), pygame.SRCALPHA)
 
 
 clock=pygame.time.Clock()
 
 running=True
 
-counter = 0
-
 wave_radius = 0
 
-wave_speed = 4
+wave_speed = 3
 
 
 last_time = pygame.time.get_ticks()
@@ -37,7 +36,7 @@ last_time = pygame.time.get_ticks()
 
 
 
-
+inc=True
 
 while running:
 
@@ -46,50 +45,34 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-            
-
-    current_time = pygame.time.get_ticks()
-
-    
-
-    if current_time - last_time >=1000:
-        
-        counter+=1
-        last_time = current_time
-
-        if counter > 10:
-            running = False
-
-
     wave_surf = pygame.Surface((600,600), pygame.SRCALPHA)
 
-    pygame.draw.circle(wave_surf, (0, 255, 255, 120), (275, 275), wave_radius, 3)
-
-            
+    pygame.draw.circle(wave_surf, (0, 0, 0, 120), (300, 300), wave_radius, 3)
 
     screen.blit(bg, (0,0))
     screen.blit(obj, (275,275))
+    screen.blit(wave_surf, (0, 0))
 
-    wave_radius += wave_speed
+    if inc==True:
+        wave_radius += wave_speed
+
+    elif inc==False:
+        wave_radius -= wave_speed
 
 
-    if wave_radius > 900:
-        wave_radius = 0
+
+
+    if wave_radius > 375:
+        inc = False
+
+    if wave_radius < 0:
+        inc = True
     
-
-    text = font.render(str(counter), True, "black")
-
-    text_rect = text.get_rect(center=(300,300))
-
-    
-        
-    screen.blit(text, text_rect)
-
     pygame.display.flip()
 
     
-
     clock.tick(60)  # limits FPS to 60
+
 
 pygame.quit()
 
